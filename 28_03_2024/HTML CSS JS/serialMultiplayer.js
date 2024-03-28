f3ec41ae30e59de574783ab31e407b497f1b5587
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2024-03-28 19:10:04 +0530

    serial multiplayer

--- 28_03_2024/HTML CSS JS/serialMultiplayer.js
+++ 28_03_2024/HTML CSS JS/serialMultiplayer.js
@@ -1,46 +1,46 @@
-class SerialMultiplier {
-    constructor(first = 1, second = 1, third = 1, fourth = 1, fifth = 1) {
-        this.first = first;
-        this.second = second;
-        this.third = third;
-        this.fourth = fourth;
-        this.fifth = fifth;
-        this.result = this.first * this.second * this.third * this.fourth * this.fifth;
-    }
+const n=4;
+const numbers=[4,5,7,2];
 
-    toString() {
+class serialMultiplayer{
+    constructor(first=1,second=1,third=1,forth=1,fifth=1){
+        this.first=first;
+        this.second=second;
+        this.third=third;
+        this.forth=forth;
+        this.fifth=fifth;
+        this.result=this.first*this.second*this.third*this.forth*this.fifth;
+    }
+    toString(){
         return this.result.toString();
     }
 }
 
-function calculateProduct(n, numbers) {
-    if (n < 1 || n > 5 || numbers.length !== n) {
-        console.error("Invalid input");
+function calculateProduct(n,numbers){
+    if(n<1||n>5||numbers.length!==n){
+        console.log('Invalid Input!');
         return;
     }
 
-    let multiplier;
-    switch (n) {
+    let multiplayer;
+
+    switch (n){
         case 1:
-            multiplier = new SerialMultiplier(numbers[0]);
+            multiplayer=new serialMultiplayer(numbers[0]);
             break;
         case 2:
-            multiplier = new SerialMultiplier(numbers[0], numbers[1]);
+            multiplayer=new serialMultiplayer(numbers[0],numbers[1]);
             break;
         case 3:
-            multiplier = new SerialMultiplier(numbers[0], numbers[1], numbers[2]);
+            multiplayer=new serialMultiplayer(numbers[0],numbers[1],numbers[2]);
             break;
         case 4:
-            multiplier = new SerialMultiplier(numbers[0], numbers[1], numbers[2], numbers[3]);
+            multiplayer=new serialMultiplayer(numbers[0],numbers[1],numbers[2],numbers[3]);
             break;
         case 5:
-            multiplier = new SerialMultiplier(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]);
+            multiplayer=new serialMultiplayer(numbers[0],numbers[1],numbers[2],numbers[3],numbers[4]);
             break;
     }
-
-    return multiplier.toString();
+    return multiplayer.toString();
 }
 
-const n = 3;
-const numbers = [1,3,4];
-console.log(calculateProduct(n, numbers));
+console.log(calculateProduct(n,numbers));
